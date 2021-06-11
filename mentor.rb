1c5c4de348666aa74e7afa45e2df7f12c2f9c49e
yuu <spare.fran626bel@gmail.com>
2021-06-11 13:46:10 +0000

    kadai-ruby-oop-2

---
+++ mentor.rb
@@ -0,0 +1,29 @@
+class Mentor
+  
+  attr_accessor :name
+  
+  def initialize(mn)
+    self.name = mn
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+class RailsMentor < Mentor
+  
+  def initialize(rm)
+    self.name = rm
+  end
+  
+  def job
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+  end
+end
+  
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+
+kirameki.job
+akaide.job
